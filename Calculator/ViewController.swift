298393a9609fef28f71f058070b849cbe451638b
Shawn Williams <jr.william09@gmail.com>
2020-10-14 10:54:08 -0500

    Got the cal buttons working with the controller. Basic setup

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,21 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var isFinishedTypingNumber: Bool = true
+    
     @IBOutlet weak var displayLabel: UILabel!
     
     
     
+    /// Handles the logic for when a user pressed a non-numeric button
+    /// - Parameter sender: The Cureent UI button sender
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
     }
 
     
+    /// Hanldes the logic for when the user pressed a number button
+    /// - Parameter sender:The UI button Sender
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
     }
 
 }
